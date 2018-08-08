68a1b286e3a8d8b387a52e56321b5e6db933d93d
iamaian <The21stKuruno@yahoo.com>
2018-08-09 07:48:43 +0900

    edited pp method

--- dist/includes/calculate.inc.php
+++ dist/includes/calculate.inc.php
@@ -3,20 +3,23 @@
     $txtNumOne = $_GET['txtNumOne'];
     $txtNumTwo = $_GET['txtNumTwo'];
     $operator = $_GET['sltOperator'];
-
-    switch ($operator) {
-      case 'Add':
-      echo $txtNumOne + $txtNumTwo;
-      break;
-      case 'Subtract':
-      echo $txtNumOne - $txtNumTwo;
-      break;
-      case 'Multiply':
-      echo $txtNumOne * $txtNumTwo;
-      break;
-      case 'Divide':
-      echo $txtNumOne / $txtNumTwo;
-      break;
+    if(empty($txtNumOne) || empty($txtNumTwo)) {
+      header("Location: ../index.php?calculate=emptyfields");
+    } else {
+      switch ($operator) {
+        case 'Add':
+        echo $txtNumOne + $txtNumTwo;
+        break;
+        case 'Subtract':
+        echo $txtNumOne - $txtNumTwo;
+        break;
+        case 'Multiply':
+        echo $txtNumOne * $txtNumTwo;
+        break;
+        case 'Divide':
+        echo $txtNumOne / $txtNumTwo;
+        break;
+      }
     }
   } else {
     header("Location: ../index.php?calculate=error");
